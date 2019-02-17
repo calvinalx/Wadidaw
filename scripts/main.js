5a787445322d08a30c33c18046726e64c9ae1d51
Calvin Alexander <calvinalx@gmail.com>
2019-02-17 16:07:36 +0700

    Wadidaw

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function toggleSearch() {
+  var searchBar = document.getElementById("search-mobile");
+  if (searchBar.style.display === "none") {
+    searchBar.style.display = "block";
+  } else {
+    searchBar.style.display = "none";
+  }
+}
